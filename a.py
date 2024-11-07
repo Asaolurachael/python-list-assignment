6cebb0c020ed74495cb951204a9071831fbed2f0
Asaolurachael <ariom6696@gmail.com>
2024-11-07 07:51:31 +0100

    Add files via upload

--- a.py
+++ a.py
@@ -1,28 +1,10 @@
 friends = ["Shindara", "mercy", "divine", "Ben", "Goodness"]
 bestfriends = ["Shindara", "Goodness"]
-commonfriends = input("Enter your name")
+name= input("Enter your name")
 
-if "Shindara" =="Shindara":
-    print("she is my friend and my bestie")
+if name in bestfriends:
+    print("she/He is my bestfriend")
+elif name in friends:
+    print("she/He is my friend")
 else:
-     print("she is my friend")
-
-if "mercy" == "mercy":
-    print("she is my friend and my bestie")
-else:
-    print("she is my friend")
-
-if "Ben" =="Ben":
-    print("He is my friend and my bestie")
-else:
-     print("He is my friend")
-
-if "Divine" == "Divine":
-    print("she is my friend and my bestie")
-else:
-    print("she is my friend")
-
-if "Goodness" =="Goodness":
-    print("she is my friend and my bestie")
-else:
-     print("she is my friend")
+    print("She/He is my friend and my bestie")
