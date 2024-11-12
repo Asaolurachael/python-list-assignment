fce56e1954bbc054a483a8c0c2880acc0e3dae27
Onlyoneariom <ariom6696@gmail.com>
2024-11-12 18:57:13 +0100

    Add files via upload

--- a.py
+++ a.py
@@ -1,10 +1,12 @@
-friends = ["Shindara", "mercy", "divine", "Ben", "Goodness"]
-bestfriends = ["Shindara", "Goodness"]
-name= input("Enter your name")
+text = ""
+letters= ["a", "b", "c", "d", "e"]
+friends = ["Bella", "Shalom","Divine", "Peace", "Joy"]       
 
-if name in bestfriends:
-    print("she/He is my bestfriend")
-elif name in friends:
-    print("she/He is my friend")
-else:
-    print("She/He is my friend and my bestie")
+print(f"{friends[0]} & {friends[-1]} are the most funniest people i have ever met.💝")
+print(f"{friends[1]} is my Childhood Bestie💓")
+print(f"{friends[3]} is the ray of sunshine who spread positivity and happiness in my life🧡")
+print(f"{friends[-1]} is one of the kindest people i've ever met💖")
+print(f"{friends[2]}, I am so lucky to have a friend like you💕 ")
+print(f"{friends[0]}, friend like you are the biggest strength that makes my life better💓")
+print(f"{friends} are the best thing that has ever happened to me💖")
+print(f"{friends}  am so glad to have you giys as my friend ❤💖😘💕")
