44bc65ecccfa2f527d9e989d15b90675354ae950
Onlyoneariom <ariom6696@gmail.com>
2025-03-09 19:11:11 +0100

    Add files via upload

---
+++ a5.py
@@ -0,0 +1,15 @@
+my__set = {1,2,2,3,4,4,4}
+print("set")
+
+my__set.add(5)
+print("Updated Set:")
+
+set1 = my__set
+set2 = {2,4,4,6}
+
+print("/nSet 1", set1)
+print("Set 2", set2)
+print("Difference")
+print("set1.difference(set2)")
+print("symmeteric Difference")
+print(set1.symmetric_difference(set2))
